696551505ea00049e70d0aeee57cee26527ecbeb
jorge <jorge@JorgeLima>
2017-10-07 21:08:30 -0300

    criaçaão classe Ingrediente no pacote Modelo

---
+++ cookbook/src/br/rj/mace/femass/cookbook/modelo/Receita.java
@@ -0,0 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.rj.mace.femass.cookbook.modelo;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ *
+ * @author YMoraes
+ */
+public class Receita {
+    private int id;
+    private String nome;
+    private String preparo;
+    private String modoServir;
+    private Categoria categoria;
+    private List<Receita_Ingrediente> ingredientes;
+    
+    public Receita(String nome){
+        this.setId(0);
+        this.setNome(nome);
+    }
+
+    public List<Receita_Ingrediente> getIngredientes() {
+        return Collections.unmodifiableList(ingredientes);
+       
+    }
+
+    public void setIngredientes(Receita_Ingrediente ingrediente) {
+        this.ingredientes.add(ingrediente);
+    }
+    
+    public void removeIngrediente(Receita_Ingrediente ingrediente){
+        if (this.ingredientes.contains(ingrediente))
+            this.ingredientes.remove(ingrediente);
+    }
+    
+    public String toString(){
+        return "Receita "+this.getNome();
+    }
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    
+    
+    public String getPreparo() {
+        return preparo;
+    }
+
+    /**
+     * @param preparo the preparo to set
+     */
+    public void setPreparo(String preparo) {
+        this.preparo = preparo;
+    }
+
+    public String getModoServir() {
+        return modoServir;
+    }
+
+    public void setModoServir(String modoServir) {
+        this.modoServir = modoServir;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria c) {
+        this.categoria = c;
+        c.setReceita(this);
+    }
+    
+    
+    
+
+}
---
+++ cookbook/src/br/rj/mace/femass/cookbook/modelo/Receita_Ingrediente.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.rj.mace.femass.cookbook.modelo;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author YMoraes
+ */
+public class Receita_Ingrediente {
+    private Receita receita;
+    private Ingrediente ingrediente;
+    private int quantidade;
+    private String unidade;
+
+    public Receita_Ingrediente() {
+    }
+        
+    public Receita getReceita() {
+        return receita;
+    }
+
+    public void setReceita(Receita receita) {
+        this.receita = receita;
+    }
+
+    public Ingrediente getIngrediente() {
+        return ingrediente;
+    }
+
+    public void setIngrediente(Ingrediente ingrediente) {
+        this.ingrediente = ingrediente;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public String getUnidade() {
+        return unidade;
+    }
+
+    public void setUnidade(String unidade) {
+        this.unidade = unidade;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Receita_Ingrediente other = (Receita_Ingrediente) obj;
+        if (!Objects.equals(this.receita, other.receita)) {
+            return false;
+        }
+        if (!Objects.equals(this.ingrediente, other.ingrediente)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    
+    
+}
